989d7af6b2222d816dfcc68d15068a36c93bb156
SHAIK BABA SHAREEF <shareefshaik63411@gmail.com>
2026-06-04 11:58:21 +0530

    Add files via upload

---
+++ day6day7/com/ArrayL.java
@@ -0,0 +1,23 @@
+package day6day7.com;
+
+
+
+import java.util.ArrayList;
+
+public class ArrayL {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(6);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.remove(3);
+       // arr.clear();
+
+
+        for(int k=0;k<arr.size();k++){
+            System.out.print(arr.get(k)+" ");
+        }
+    }
+}
